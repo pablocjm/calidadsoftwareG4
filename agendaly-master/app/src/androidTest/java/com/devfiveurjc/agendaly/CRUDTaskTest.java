cf4ce7cb8714d21654d4ffb98df4c87fa25666a6
Akram <80918031+Akram1013@users.noreply.github.com>
2023-04-19 10:40:57 +0200

    Update CRUDTaskTest.java
    Test de DeleteTask y UpdateTask

--- agendaly-master/app/src/androidTest/java/com/devfiveurjc/agendaly/CRUDTaskTest.java
+++ agendaly-master/app/src/androidTest/java/com/devfiveurjc/agendaly/CRUDTaskTest.java
@@ -21,5 +21,33 @@ public class CRUDTaskTest {
         Assert.assertEquals(1, tasks.size());
         Assert.assertEquals("hola", tasks.get(0).getTitle());
     }
+    
+    @Test
+    public void testDeleteTask(){
+        //Borrar todas las Task
+        CRUDTask.deleteAllTasks();
+
+        CRUDTask.addTask(new Task("hola", "que tal", new Date()));
+        List<Task> tasks = CRUDTask.getAllTasks();
+        int id = tasks.get(0).getId();
+
+        CRUDTask.deleteTask(id);
+        tasks = CRUDTask.getAllTasks();
+        Assert.assertEquals(0, tasks.size());
+    }
+
+    @Test
+    public void testUpdateTask(){
+        //Borrar todas las Task
+        CRUDTask.deleteAllTasks();
+
+        CRUDTask.addTask(new Task("hola", "que tal", new Date()));
+        List<Task> tasks = CRUDTask.getAllTasks();
+        Task newTask = new Task("hey", "esto es nuevo", new Date());
+        CRUDTask.updateTask(tasks.get(0), newTask);
+
+        Assert.assertEquals("esto es nuevo", tasks.get(0).getDescription());
+        Assert.assertEquals("hey", tasks.get(0).getTitle());
+    }
 
 }
