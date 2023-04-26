8e72162fd46561c451c12580c6ec20900eac0591
Antonio Pajuelo <a.pajuelo1999@hotmail.com>
2023-04-26 13:41:58 +0200

    Navigation test completed

---
+++ agendaly-master/app/src/androidTest/java/com/devfiveurjc/agendaly/NavigationTest.java
@@ -0,0 +1,44 @@
+package com.devfiveurjc.agendaly;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+
+import com.devfiveurjc.agendaly.activities.MainActivity;
+
+
+import org.junit.Test;
+
+
+public class NavigationTest {
+
+    @Test
+    public void testNavigationCreated() {
+        ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.addFloatingButton)).perform(click());
+        onView(withId(R.id.taskAddDate)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testNavigationEdit() {
+        ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.listRecyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+        onView(withId(R.id.taskInfoEditButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testNavigationSettings() {
+        ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.listRecyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0,click()));
+        onView(withId(R.id.taskInfoEditButton)).perform(click());
+        onView(withId(R.id.taskEditSaveButton)).check(matches(isDisplayed()));
+    }
+
+}
